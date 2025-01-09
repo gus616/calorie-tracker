620f325ba97791bf7e3a0231eef391b2eeda5bdd
Gus Vc <guzz.vc@gmail.com>
2025-01-09 12:18:39 -0600

    calorie display conditional rendering added

--- src/App.tsx
+++ src/App.tsx
@@ -11,22 +11,26 @@ function App() {
 
   useEffect(() => {
     localStorage.setItem('activities', JSON.stringify(state.activities));
-   }, [state.activities]);
+  }, [state.activities]);
 
-   const isActivitiesListEmpty = state.activities.length === 0 ? true : false;
+  const isActivitiesListEmpty = state.activities.length === 0 ? true : false;
 
   return (
     <>
-      <Header dispatch={dispatch} activitiesEmpty = {isActivitiesListEmpty} />
+      <Header dispatch={dispatch} activitiesEmpty={isActivitiesListEmpty} />
       <section className="bg-lime-500 py-20 px-5">
         <CalorieForm dispatch={dispatch} state={state} />
       </section>
+      {
+        state.activities.length > 0 &&
+        <section className="bg-gray-800 py-10">
 
-      <section className="bg-gray-800 py-10">
-        <div className="max-w-4xl mx-auto">
-          <CalorieTracker  activities={state.activities}/>
-        </div>
-      </section>
+          <div className="max-w-4xl mx-auto">
+            <CalorieTracker activities={state.activities} />
+          </div>
+
+        </section>
+      }
 
       <section className="bg-gray-200 min-h-screen">
         <ActivityList activities={state.activities} dispatch={dispatch} />
